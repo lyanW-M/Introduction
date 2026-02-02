3eecc3873314cffc6f9f5520a8e905838bbb6d3e
lyanW-M <lyanw@wsc.school.nz>
2026-02-03 12:13:12 +1300

    a

---
+++ text.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Why did you choose Digital Technology?";
+    std::cout << "Because i think its fun to learn how to create things on the computer or make something irl from out of the computer";
+
+    std::cout << "What do you want to do after you leave school?";
+    std::cout << "I just want a job that can help me get a simple and quiet life in an apartment";
+
+    std::cout << "What is your most/least favorite subject?";
+    std::cout << "I hate media studies the most rn and I love English because I get to sleep when I'm done with the work";
+
+    std::cout << "How do you work best?";
+    std::cout << "I like a task that's fun but if it's a boring one, I usually just fall asleep";
+
+    return 0;
+}
